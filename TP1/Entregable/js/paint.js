107e9b9d3845d85d68a1fe0ff20ffa3b0ca3880e
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2021-09-05 21:17:03 -0300

    agregando funciones de filtros de grises de prueba

--- TP1/Entregable/js/paint.js
+++ TP1/Entregable/js/paint.js
@@ -1,55 +1,48 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     "use strict"
-    
-    window.onload = function() {
+
+    window.onload = function () {
         let canvas = document.querySelector('#canvas');
+        let width = canvas.width;
+        let height = canvas.height;
+
+        
+        let Gris = document.querySelector('#btnFiltroGris');
+        let Brillo = document.querySelector('#btnFiltroBrillo');
+        let Sepia = document.querySelector('#btnFiltroSepia');
+        let Blur = document.querySelector('#btnFiltroBlur');
+        let Binarizacion = document.querySelector('#btnFiltroBinarizacion');
+        let Saturacion = document.querySelector('#btnFiltroSaturacion');
+
         let ctx = canvas.getContext('2d');
         let ruta = false;
         let borrado = -1;
-        let x , y;
- 
-        /********   SUBIR IMAGENES    *********/
-        
-        let inputImage = document.querySelector("#selectImage");
-        inputImage.addEventListener("change", handleFiles, false);
-
-        function handleFiles() {
-            let reader = new FileReader();
-            reader.onload = function(){
-                let image = new Image();
-                image.onload = function(){                  
-                    ctx.drawImage(image,0,0);
-                }
-                image.src = reader.result;
-            }
-            reader.readAsDataURL(inputImage.files[0]);
-        }
-
+        let x, y;
 
         /*************   PAINT   ************/
 
-        canvas.addEventListener('mousedown', function(){ // evento que se activa al dar click
+        canvas.addEventListener('mousedown', function () { // evento que se activa al dar click
             ruta = true;
             ctx.beginPath(); //comienza a dibujar
-            ctx.moveTo(x,y); //cordenadas iniciales
+            ctx.moveTo(x, y); //cordenadas iniciales
             canvas.addEventListener('mousemove', dibujarLapiz);
         });
 
-        function dibujarLapiz(evento){
+        function dibujarLapiz(evento) {
             x = evento.layerX;
             y = evento.layerY;
-            if(ruta){ 
-                if(borrado==1){
+            if (ruta) {
+                if (borrado == 1) {
                     let lineSize = document.querySelector('#range-erase').value;
-                    let lineColour = document.querySelector('#inputColors').value;
-                    ctx.lineTo(x,y); // genera linea
+                    // let lineColour = document.querySelector('#inputColors').value;
+                    ctx.lineTo(x, y); // genera linea
                     ctx.lineWidth = lineSize;
                     ctx.strokeStyle = 'white';
                     ctx.stroke();
-                }else{
+                } else {
                     let lineSize = document.querySelector('#range-line').value;
                     let lineColour = document.querySelector('#inputColors').value;
-                    ctx.lineTo(x,y); // genera linea
+                    ctx.lineTo(x, y); // genera linea
                     ctx.lineWidth = lineSize;
                     ctx.strokeStyle = lineColour;
                     ctx.stroke(); //para que se dibuje la linea
@@ -59,19 +52,55 @@ document.addEventListener('DOMContentLoaded', () =>{
 
         canvas.addEventListener('mousemove', dibujarLapiz);
 
-        canvas.addEventListener('mouseup', function(){
+        canvas.addEventListener('mouseup', function () {
             ruta = false;
         });
 
         let imgBorrar = document.querySelector('#erase');
-        imgBorrar.addEventListener('click', function(){
+        imgBorrar.addEventListener('click', function () {
             borrado = borrado * -1;
         });
 
         let btnBorrar = document.querySelector('#borrar');
-        btnBorrar.addEventListener('click', function() {
+        btnBorrar.addEventListener('click', function () {
             ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         });
 
-    };
-});
+
+        /********   SUBIR IMAGENES    *********/
+
+        let inputImage = document.querySelector("#selectImage");
+        inputImage.addEventListener("change", function(e){
+            let reader = new FileReader();
+            reader.onload = function () {
+                var image = new Image();
+                image.onload = function () {
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+                    Gris.addEventListener('click', () => {
+                        filterImageGrey(image, width, height);
+                    })
+                }
+                image.src = reader.result;
+            }
+            reader.readAsDataURL(inputImage.files[0]);
+        }, false);
+
+
+            /*********  FILTROS  ********/
+            function filterImageGrey(image, width, height) {
+                var imageData = ctx.getImageData(0, 0, width, height);
+                let data = imageData.data;
+                for (let i = 0; i < data.length; i += 4) {
+                    let grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                    data[i] = grey;
+                    data[i + 1] = grey;
+                    data[i + 2] = grey;
+                    data[i + 3] = 255;
+                }
+    
+                ctx.putImageData(imageData, 0, 0);
+            }
+        };
+    });
