cd346ad5c812a6faf77acb759a8ce2df602128f4
Martin Lopardo <tincho921007cs@gmail.com>
2021-09-06 00:22:42 -0300

    Agrego btn filtroNegativo

--- TP1/Entregable/js/paint.js
+++ TP1/Entregable/js/paint.js
@@ -3,14 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.onload = function () {
         let canvas = document.querySelector('#canvas');
-
+        let negativo = document.querySelector('#btnFiltroNegativo');
         let gris = document.querySelector('#btnFiltroGris');
         let brillo = document.querySelector('#btnFiltroBrillo');
         let sepia = document.querySelector('#btnFiltroSepia');
         let blur = document.querySelector('#btnFiltroBlur');
         let binarizacion = document.querySelector('#btnFiltroBinarizacion');
         let saturacion = document.querySelector('#btnFiltroSaturacion');
-
         let ctx = canvas.getContext('2d');
         let ruta = false;
         let borrado = -1;
@@ -31,7 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
             if (ruta) {
                 if (borrado == 1) {
                     let lineSize = document.querySelector('#range-erase').value;
-                    // let lineColour = document.querySelector('#inputColors').value;
                     ctx.lineTo(x, y); // genera linea
                     ctx.lineWidth = lineSize;
                     ctx.strokeStyle = 'white';
@@ -75,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     canvas.width = image.width;
                     canvas.height = image.height;
                     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-                    botonesFiltros(image, canvas.width, canvas.height);
+                    botonesFiltros(image);
                 }
                 image.src = reader.result;
             }
@@ -83,32 +81,50 @@ document.addEventListener('DOMContentLoaded', () => {
         }, false);
 
 
-        function botonesFiltros(image, width, height) {
+        function botonesFiltros(image) {
             gris.addEventListener('click', () => {
-                filterImageGrey(image, width, height);
+                filterImageBinario(image);
             });
             brillo.addEventListener('click', () => {
-                filterImageBrillo(image, width, height);
+                filterImageBrillo(image);
+            });
+            negativo.addEventListener('click', () => {
+                filterImageNegativo(image);
             });
             sepia.addEventListener('click', () => {
-                filterImageSepia(image, width, height);
+                filterImageSepia(image);
             });
             blur.addEventListener('click', () => {
                 filterImageBlur(image, width, height);
             });
             binarizacion.addEventListener('click', () => {
-                filterImageBinarizacion(image, width, height);
+                filterImageBinarizacion(image);
             });
             saturacion.addEventListener('click', () => {
-                filterImageSaturacion(image, width, height);
+                filterImageSaturacion(image);
             });
         }
 
-        function filterImageGrey(image, width, height) {
-            let imageData = ctx.getImageData(0, 0, width, height);
+        function filterImageNegativo(image) {
+            let imageData = ctx.getImageData(0, 0, image.width, image.height);
+            let data = imageData.data;
+            for (let i = 0; i < data.length; i +=4) {
+                let r = data[i];
+                let g = data[i + 1];
+                let b = data[i + 2];
+                data[i] = 255 - r;
+                data[i + 1] = 255 - g;
+                data[i + 2] = 255 - b;
+                data[i + 3] = 255;
+            }
+            ctx.putImageData(imageData, 0, 0);
+        }
+
+        function filterImageBinario(image) {
+            let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
-                let grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
+                let gray = (data[i] + data[i + 1] + data[i + 2]) / 3;
                 data[i] = grey;
                 data[i + 1] = grey;
                 data[i + 2] = grey;
@@ -117,67 +133,47 @@ document.addEventListener('DOMContentLoaded', () => {
             ctx.putImageData(imageData, 0, 0);
         }
 
-        function filterImageBrillo(image, width, height) {
-            let imageData = ctx.getImageData(0, 0, width, height);
+        function filterImageBrillo(image) {
+            let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
-                let grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                data[i] = grey;
-                data[i + 1] = grey;
-                data[i + 2] = grey;
-                data[i + 3] = 255;
+               
             }
             ctx.putImageData(imageData, 0, 0);
         }
 
-        function filterImageSepia(image, width, height) {
-            let imageData = ctx.getImageData(0, 0, width, height);
+        function filterImageSepia(image) {
+            let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
-                let grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                data[i] = grey;
-                data[i + 1] = grey;
-                data[i + 2] = grey;
-                data[i + 3] = 255;
+               
             }
             ctx.putImageData(imageData, 0, 0);
         }
 
-        function filterImageBlur(image, width, height) {
-            let imageData = ctx.getImageData(0, 0, width, height);
+        function filterImageBlur(image) {
+            let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
-                let grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                data[i] = grey;
-                data[i + 1] = grey;
-                data[i + 2] = grey;
-                data[i + 3] = 255;
+                
             }
             ctx.putImageData(imageData, 0, 0);
         }
 
-        function filterImageBinarizacion(image, width, height) {
-            let imageData = ctx.getImageData(0, 0, width, height);
+        function filterImageBinarizacion(image) {
+            let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
-                let grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                data[i] = grey;
-                data[i + 1] = grey;
-                data[i + 2] = grey;
-                data[i + 3] = 255;
+
             }
             ctx.putImageData(imageData, 0, 0);
         }
         
-        function filterImageSaturacion(image, width, height) {
-            let imageData = ctx.getImageData(0, 0, width, height);
+        function filterImageSaturacion(image) {
+            let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
-                let grey = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                data[i] = grey;
-                data[i + 1] = grey;
-                data[i + 2] = grey;
-                data[i + 3] = 255;
+
             }
             ctx.putImageData(imageData, 0, 0);
         }
