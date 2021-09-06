a3089a80a61449cea27c24fab6223ec15151dd81
Martin Lopardo <tincho921007cs@gmail.com>
2021-09-06 13:36:51 -0300

    Agrego comentarios a las funciones, cambio algunos nombres de variables por otros + representativos

--- TP1/Entregable/js/paint.js
+++ TP1/Entregable/js/paint.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.onload = function () {
         let canvas = document.querySelector('#canvas');
+        let ctx = canvas.getContext('2d');
+        let ruta = false;
+        let erase = -1;
+        let x, y;
+        //botones de filtros
         let negativo = document.querySelector('#btnFiltroNegativo');
         let gris = document.querySelector('#btnFiltroGris');
         let brillo = document.querySelector('#btnFiltroBrillo');
@@ -10,36 +15,32 @@ document.addEventListener('DOMContentLoaded', () => {
         let blur = document.querySelector('#btnFiltroBlur');
         let binarizacion = document.querySelector('#btnFiltroBinarizacion');
         let saturacion = document.querySelector('#btnFiltroSaturacion');
-        let ctx = canvas.getContext('2d');
-        let ruta = false;
-        let borrado = -1;
-        let x, y;
 
         /*************   PAINT   ************/
 
-        canvas.addEventListener('mousedown', function () { // evento que se activa al dar click
-            ruta = true;
+        canvas.addEventListener('mousedown', function () { // evento que se activa al mantener clickeado (click izq)
+            ruta = true; //si se mantiene apretado el click en el canvas, ruta = true
             ctx.beginPath(); //comienza a dibujar
             ctx.moveTo(x, y); //cordenadas iniciales
-            canvas.addEventListener('mousemove', dibujarLapiz);
+            canvas.addEventListener('mousemove', dibujarLapiz); //evento al mover el mouse
         });
 
         function dibujarLapiz(evento) {
             x = evento.layerX;
             y = evento.layerY;
             if (ruta) {
-                if (borrado == 1) {
-                    let lineSize = document.querySelector('#range-erase').value;
+                if (erase == 1) {
+                    let eraseLineSize = document.querySelector('#range-erase').value;
                     ctx.lineTo(x, y); // genera linea
-                    ctx.lineWidth = lineSize;
+                    ctx.lineWidth = eraseLineSize;
                     ctx.strokeStyle = 'white';
                     ctx.stroke();
                 } else {
-                    let lineSize = document.querySelector('#range-line').value;
-                    let lineColour = document.querySelector('#inputColors').value;
+                    let pencilLineSize = document.querySelector('#range-line').value;
+                    let penciLineColour = document.querySelector('#inputColors').value;
                     ctx.lineTo(x, y); // genera linea
-                    ctx.lineWidth = lineSize;
-                    ctx.strokeStyle = lineColour;
+                    ctx.lineWidth = pencilLineSize;
+                    ctx.strokeStyle = penciLineColour;
                     ctx.stroke(); //para que se dibuje la linea
                 }
             }
@@ -51,13 +52,17 @@ document.addEventListener('DOMContentLoaded', () => {
             ruta = false;
         });
 
-        let imgBorrar = document.querySelector('#erase');
-        imgBorrar.addEventListener('click', function () {
-            borrado = borrado * -1;
+        let btnErase = document.querySelector('#btnErase');
+        //se le aplica un evento al boton erase (al clickearlo)
+        btnErase.addEventListener('click', function () {
+            btnErase.classList.toggle('toggle');
+            erase = erase * -1;
         });
 
-        let btnBorrar = document.querySelector('#borrar');
-        btnBorrar.addEventListener('click', function () {
+        let btnDescartar = document.querySelector('#descartar');
+        //se le aplica un evento al boton descartar (al clickearlo)
+        btnDescartar.addEventListener('click', function () {
+            //borra todo lo que hay en el canvas
             ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         });
 
@@ -65,13 +70,17 @@ document.addEventListener('DOMContentLoaded', () => {
         /********   SUBIR IMAGENES Y FILTROS   *********/
 
         let inputImage = document.querySelector("#selectImage");
-        inputImage.addEventListener("change", function (e) {
+        //se aplica un evento change al input de la imagen, el cual se dispara al cambiar el valor del input (al seleccionar una foto)
+        inputImage.addEventListener("change", function () {
+            //se crea la variable reader, que permite leer ficheros almacenados en el cliente de forma asincronica usando el objeto file
             let reader = new FileReader();
             reader.onload = function () {
                 var image = new Image();
                 image.onload = function () {
+                    //setea el tamaño del canvas igual al tamaño de la imagen
                     canvas.width = image.width;
                     canvas.height = image.height;
+                    //dibuja la imagen en el canvas
                     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
                     botonesFiltros(image);
                 }
@@ -80,10 +89,10 @@ document.addEventListener('DOMContentLoaded', () => {
             reader.readAsDataURL(inputImage.files[0]);
         }, false);
 
-
+        //asigna el evento click a los botones de filtros
         function botonesFiltros(image) {
             gris.addEventListener('click', () => {
-                filterImageBinario(image);
+                filterImageGrises(image)
             });
             brillo.addEventListener('click', () => {
                 filterImageBrillo(image);
@@ -105,6 +114,11 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
 
+        /*filtro que recorre la imagen pasada por parametro e invierte los valores r,g,b
+        (EJ: si, data[0] = 0
+             data[0] = 255 - 0
+             data[0] = 255 (se invierte, pasa de blanco a negro))
+        */
         function filterImageNegativo(image) {
             let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
@@ -115,24 +129,27 @@ document.addEventListener('DOMContentLoaded', () => {
                 data[i] = 255 - r;
                 data[i + 1] = 255 - g;
                 data[i + 2] = 255 - b;
-                data[i + 3] = 255;
+                data[i + 3] = 255; //sin transparencia
             }
             ctx.putImageData(imageData, 0, 0);
         }
 
-        function filterImageBinario(image) {
+        /* filtro que recorre la imagen pasada por parametro y le aplica distintas tonalidades 
+           de grises a los pixeles.*/
+        function filterImageGrises(image) {
             let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
             for (let i = 0; i < data.length; i += 4) {
-                let gray = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                data[i] = grey;
-                data[i + 1] = grey;
-                data[i + 2] = grey;
-                data[i + 3] = 255;
+                let gray = (data[i] + data[i + 1] + data[i + 2]) / 3; // toma los valores rgb, los suma y al dividirlos por 3 va generando distintas tonalidades de grises dependiendo de los colores de cada iteracion (EJ: 80+100+30=210/3=70) entonces le aplicamos 70 a r,g y b para que quede esa tonalidad de gris
+                data[i] = gray;
+                data[i + 1] = gray;
+                data[i + 2] = gray;
+                data[i + 3] = 255; //sin transparencia
             }
             ctx.putImageData(imageData, 0, 0);
         }
 
+        /* */
         function filterImageBrillo(image) {
             let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
@@ -142,6 +159,7 @@ document.addEventListener('DOMContentLoaded', () => {
             ctx.putImageData(imageData, 0, 0);
         }
 
+        /* */
         function filterImageSepia(image) {
             let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
@@ -151,6 +169,7 @@ document.addEventListener('DOMContentLoaded', () => {
             ctx.putImageData(imageData, 0, 0);
         }
 
+        /* */
         function filterImageBlur(image) {
             let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
@@ -160,6 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
             ctx.putImageData(imageData, 0, 0);
         }
 
+        /**/
         function filterImageBinarizacion(image) {
             let imageData = ctx.getImageData(0, 0, image.width, image.height);
             let data = imageData.data;
