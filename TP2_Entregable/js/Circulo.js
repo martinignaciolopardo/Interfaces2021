1a7f34192d6ad29ff3fa0d835d45e19ebe7592dc
Martin Lopardo <tincho921007cs@gmail.com>
2021-09-17 20:40:14 -0300

    agrego clase circulo JS

---
+++ TP2_Entregable/js/Circulo.js
@@ -0,0 +1,25 @@
+class Circulo extends Figura{
+    constructor(posX, posY, radio, fill, ctx){
+        super(posX, posY, fill, ctx);
+        this.radio = radio;
+    }
+
+    draw(){
+        super.draw();
+        this.ctx.beginPath();
+        this.ctx.arc(this.posX, this.posY, this.radio, 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+
+    getRadio(){
+        return this.radio;
+    }
+
+    mouseEnCirculo(x, y){
+        let _x = this.posX -x;
+        let _y = this.posY -y;
+        return Math.sqrt(_x * _x + _y * _y) < this.radio;
+    }
+
+}
