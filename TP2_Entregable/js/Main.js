9db09afe891294a3558d694c2a4a37a15c684726
Martin Lopardo <tincho921007cs@gmail.com>
2021-09-20 15:32:59 -0300

    Agrego funcion para agregar fichas y para moverlas. agrego al dibujar el tablero, que guarde las posiciones de las ranuras

--- TP2_Entregable/js/Main.js
+++ TP2_Entregable/js/Main.js
@@ -1,40 +1,103 @@
 document.addEventListener('DOMContentLoaded', () => {
     let canvas = document.querySelector('#canvas');
     let ctx = canvas.getContext('2d');
-    let canvasWidth = window.innerWidth -50;
-    let canvasHeight = window.innerHeight -50;
-    canvas.height = 600;
-    canvas.width = 900;
-
-    function agregarCirculo(){
-        let radio = 30;
-        let posX = Math.random() * (canvasWidth - radio * 2) + radio;
-        let posY = Math.random() * (canvasHeight - radio *2) + radio;
-        let color = 'red';
-        let circulo1 = new Circulo(posX, posY, radio, color, ctx);
-        circulo1.draw();
-    }
-        //agregarCirculo();
+    let canvasWidth = window.innerWidth - 30;
+    let canvasHeight = window.innerHeight - 30;
+    canvas.height = canvasHeight;
+    canvas.width = canvasWidth;
+    //jugar minimo en 980px x 625px
+    let fichaArray = [];
+    let mouseDown = false;
 
     /* dibuja tablero */
     function dibujarTablero(){
-        let posX = 100;
+        let posX = 250;
         let posY = 50;
-        radio = 30;
-        color = "gray";
-        ancho = 8;
-        alto = 5;
+        let radio = 30;
+        let color = "gray";
+        let ancho = 7;
+        let alto = 6;
+        let posiciones = [];
         for (let x = 0; x < alto; x++) {
-            posY += 90;
-            posX = 100;
+            posY += 75;
+            posX = 250;
             for (let y = 0; y < ancho; y++) {
-                let circulo = new Circulo(posX, posY, radio, color, ctx);
-                posX += 90;
-                circulo.draw();
+                let coordenada = new Circulo(posX, posY, radio, color, ctx);
+                posiciones.push(coordenada);
+                coordenada.draw();
+                posX += 75;
             }
         }
+        console.log(posiciones);
     }
     dibujarTablero();
 
+    function agregarFicha(){
+        
+        let radio = 30;
+        let color = 'red';
+        let cantidadFichas = 40;
+        for (let i = 0; i < cantidadFichas; i++) {
+            let posX = Math.floor(Math.random() * (150 - 40 + 1) + 40); //(Math.random() * (canvasWidth - radio * 2) + radio)/5;
+            let posY = Math.random() * (canvasHeight - radio * 2) + radio;
+            if (i >= cantidadFichas/2) {
+                color = 'yellow';
+                posX = Math.floor(Math.random() * (900 - 780 + 1) + 780); //Math.random() * (canvasWidth - radio * 2) + radio;
+                posY = Math.random() * (canvasHeight - radio *2) + radio;
+            }
+            fichaArray.push(new Circulo(posX, posY, radio, color, ctx));
+        }
+        for (let y = 0; y < fichaArray.length; y++) {
+            fichaArray[y].draw();
+        }
+        console.log(fichaArray);
+    }
+    agregarFicha();
+
+    function fichaClickeada(x,y){
+        for (let i = 0; i < fichaArray.length; i++) {
+            const ficha = fichaArray[i];
+            if (ficha.mouseEnCirculo(x,y)) {
+                return ficha;
+            }
+        }
+    }
+
+    function onMouseDown(e){
+        mouseDown = true;
+        let fichaClick = fichaClickeada(e.layerX, e.layerY);
+        if (fichaClick != null) {
+            fichaClickeadaActual = fichaClick;
+        }
+    }
+
+    function onMouseUp(){
+        mouseDown = false;
+    }
+
+    function onMouseMove(e){
+        if (mouseDown  && fichaClickeadaActual != null) {
+            fichaClickeadaActual.setPosition(e.layerX, e.layerY);
+            dibujar();
+        }
+    }
+
+    function dibujar(){
+        limpiarCanvas();
+        dibujarTablero();
+        for (let i = 0; i < fichaArray.length; i++) {
+            fichaArray[i].draw();
+        }
+    }
+
+    function limpiarCanvas(){
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    }
+
+
+    canvas.addEventListener('mousedown', onMouseDown, false);
+    canvas.addEventListener('mouseup', onMouseUp, false);
+    canvas.addEventListener('mousemove', onMouseMove, false);
 
 });
