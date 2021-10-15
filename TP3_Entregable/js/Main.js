daf93c4b38e276cfa63c7c774c3abfcf5da055ab
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2021-10-15 17:05:55 -0300

    agregando prueba loop e imagenes de flechas de uso

--- TP3_Entregable/js/Main.js
+++ TP3_Entregable/js/Main.js
@@ -2,9 +2,23 @@ document.addEventListener('DOMContentLoaded',() => {
 
     let canvas = document.querySelector('#canvas');
     let ctx =  canvas.getContext('2d');
+    let tiempoLoop = 0;
+    let salto = true;
+    let gameloop = window.setInterval(accion,1000);
     let width = canvas.width;
     let height = canvas.height;
     let img = new Image();
+
+
+    function accion(){
+        if(tiempoLoop < 10){
+            console.log('x = '+ tiempoLoop );
+            tiempoLoop ++;
+        }else{
+            window.clearInterval(gameloop);
+            console.log('Listo');
+        }
+    }
 
     function setBackground(){
         ctx.save();
