226d23622acd8a32785032ace7fb89c838929216
Martin Lopardo <tincho921007cs@gmail.com>
2021-10-16 16:46:41 -0300

    Agrego comentarios

--- TP3_Entregable/js/Main.js
+++ TP3_Entregable/js/Main.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded',() => {
     let divPersonaje = document.querySelector("#personaje");
-    let tiempoLoop = 0;
-    //let salta = false;
+    //let tiempoLoop = 0;
     //let gameloop = window.setInterval(accion,1000);
     caminar();
     
     function saltar(e){
+        //si se presiona la tecla (flecha hacia arriba), se le saca la clase caminar
+        //al div donde esta el personaje y se le aplica la clase saltar
         if (e.keyCode == 38) {
-            //salta = true;
             divPersonaje.classList.remove("caminar");
             divPersonaje.classList.add("saltar");
-            //setTimeout(caminar,1000);
         }
     }
 
-    divPersonaje.addEventListener('animationend', () =>{
-       // salta = false;
-        divPersonaje.classList.remove("saltar");
-        divPersonaje.classList.add("caminar");
+    //el evento animationend detecta cuando una animacion finaliza
+    //si la animacion es la de saltar, se le quita la misma al div
+    //donde esta el personaje y se le aplica la clase caminar, para que 
+    //de esta forma solo salte 1 vez y siga caminando
+    divPersonaje.addEventListener('animationend', (e) =>{
+        console.log(e);
+        if (e.animationName == "saltar") {
+            divPersonaje.classList.remove("saltar");
+            divPersonaje.classList.add("caminar");
+        }
+       
     })
     
     function caminar(){
-        //salta = false;
         divPersonaje.classList.remove("saltar");
         divPersonaje.classList.add("caminar");
     }
@@ -34,12 +39,8 @@ document.addEventListener('DOMContentLoaded',() => {
             window.clearInterval(gameloop);
             console.log('Listo');
         }
-    }
-
-    img.onload = function(){
-        ctx.drawImage(img,100,350,170,100);
-    }
-    img.src = "images/caracol.png"*/
+    }*/
     
     window.addEventListener("keydown", saltar);
+
 });
