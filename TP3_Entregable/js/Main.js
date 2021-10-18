7f6aa733de948a201ae4d0fb7bff48a43bcd92d7
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2021-10-17 23:18:40 -0300

    agregando imagenes de inicio de partida

--- TP3_Entregable/js/Main.js
+++ TP3_Entregable/js/Main.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
     let divPersonaje = document.querySelector("#personaje");
     let divPasto = document.querySelector("#pasto");
     let divCielo = document.querySelector("#cielo");
     let divArboles = document.querySelector("#arboles");
     let divMontanias = document.querySelector("#montanias");
     let divGolem = document.querySelector("#golem");
+    let divInicioCartel = document.querySelector("#inicioCartel");
     let divStalagtita = document.querySelector("#stalagtita");
     let saltando = false;
 
-    //let tiempoLoop = 0;
-    //let gameloop = window.setInterval(accion,1000);
-    
-    function saltar(e){
+    let tiempoLoop = 0;
+    let gameloop = window.setInterval(accion,1000);
+
+    function saltar(e) {
         //si se presiona la tecla (flecha hacia arriba), se le saca la clase caminar
         //al div donde esta el personaje y se le aplica la clase saltar
         if (e.keyCode == 38) {
@@ -26,25 +27,25 @@ document.addEventListener('DOMContentLoaded',() => {
     //si la animacion es la de saltar, se le quita la misma al div
     //donde esta el personaje y se le aplica la clase caminar, para que 
     //de esta forma solo salte 1 vez y siga caminando
-    divPersonaje.addEventListener('animationend', (e) =>{
+    divPersonaje.addEventListener('animationend', (e) => {
         console.log(e);
         if (e.animationName == "saltar") {
             saltando = false;
             divPersonaje.classList.remove("saltar");
             divPersonaje.classList.add("caminar");
-        }else if(e.animationName == "agachar"){
+        } else if (e.animationName == "agachar") {
             divPersonaje.classList.remove("agachar");
             divPersonaje.classList.add("caminar");
         }
-       
+
     })
 
-    divGolem.addEventListener('animationstart', (e) =>{
+    divGolem.addEventListener('animationstart', (e) => {
         console.log(e);
         divGolem.classList.add('golemImg');
     })
-    
-    function caminar(e){
+
+    function caminar(e) {
         console.log(e);
         if (e.keyCode == 39 && saltando == false) {
             divPersonaje.classList.remove("saltar");
@@ -58,7 +59,7 @@ document.addEventListener('DOMContentLoaded',() => {
         }
     }
 
-    function agachar(e){
+    function agachar(e) {
         console.log(e);
         if (e.keyCode == 40) {
             divPersonaje.classList.remove("caminar");
@@ -66,7 +67,7 @@ document.addEventListener('DOMContentLoaded',() => {
         }
     }
 
-    function morir(e){
+    function morir(e) {
         if (e.keyCode == 37) {
             /*divPersonaje.classList.remove("saltar");*/
             //divPersonaje.style.animationPlayState = 'paused';
@@ -86,24 +87,34 @@ document.addEventListener('DOMContentLoaded',() => {
         }
     }
 
-    function pause(){
+    function pause() {
         divPersonaje.style.animationPlayState = 'paused';
     }
 
-    /*function accion(){
-        if(tiempoLoop < 10){
-            console.log('x = '+ tiempoLoop );
-            tiempoLoop ++;
-        }else{
-            window.clearInterval(gameloop);
-            console.log('Listo');
+    function accion(e) {
+            if(e.keyCode == 40){
+
+            if (tiempoLoop < 10) {
+                divInicioCartel.classList.add("inicioCartel");
+                divInicioCartel.classList.add("areYouReady");
+                if (tiempoLoop == 3) {
+                    divInicioCartel.classList.remove("areYouReady");
+                    divInicioCartel.classList.add("letsGo");
+                }
+                tiempoLoop++;
+            }else {
+                window.clearInterval(gameloop);
+                divInicioCartel.classList.remove("inicioCartel");
+                divInicioCartel.classList.remove("letsGo")
+                console.log('Listo');
+            }
         }
-    }*/
+    }
 
     window.addEventListener("keydown", caminar);
     window.addEventListener("keydown", saltar);
     window.addEventListener("keydown", agachar);
     window.addEventListener("keydown", morir);
-    
-    
+
+
 });
