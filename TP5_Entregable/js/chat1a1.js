800e115820775b1d56035544632a32ad56b83dfd
Martin Lopardo <tincho921007cs@gmail.com>
2021-12-02 18:14:59 -0300

    Detalles msj audio

--- TP5_Entregable/js/chat1a1.js
+++ TP5_Entregable/js/chat1a1.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let mensaje = document.querySelector("#mensaje");
     let archivo = document.querySelector("#archivo");
     let camara = document.querySelector("#camara");
-    let audio2 = document.querySelector("#audio2");
+    let audio = document.querySelector("#audio");
     let backToChat = document.querySelector("#nav-back");
     enviar.classList.add("oculto");
 
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         enviar.classList.toggle("oculto");
         archivo.classList.toggle("oculto");
         camara.classList.toggle("oculto");
-        audio2.classList.toggle("oculto");
+        audio.classList.toggle("oculto");
         mensaje.value = '';
     }
 
